84b5a56ad3ed08236d38f4c799696920a98bb6ca
Tony Jaeger <tjaeger@quiktrak.prv>
2017-01-13 09:49:44 -0800

    pin bar and windows rsa updates

--- ITOps_Desktop/tony_desktop.ps1
+++ ITOps_Desktop/tony_desktop.ps1
@@ -7,32 +7,36 @@ Update-ExecutionPolicy Unrestricted
 Set-TaskbarOptions -Size Large -Lock -Dock Bottom
 
 
-choco install sublimetext3
-choco install notepadplusplus.install
-choco install git
-choco install adobereader
-choco install GoogleChrome
-choco install 7zip.install
-choco install windirstat
-choco install visualstudiocode
-choco install Firefox
-choco install putty
-choco install virtualbox
-choco install teamviewer
-choco install python
-choco install winscp
-choco install vagrant
-choco install rdcman
+cinst sublimetext3
+cinst notepadplusplus.install
+cinst git
+cinst adobereader
+cinst GoogleChrome
+cinst 7zip.install
+cinst windirstat
+cinst visualstudiocode
+cinst Firefox
+cinst putty
+cinst virtualbox
+cinst teamviewer
+cinst python
+cinst winscp
+cinst vagrant
+cinst rdcman
 
 
 Install-WindowsUpdate -AcceptEula
 
 #remove edge from taskbar
 #pin chrome to taskbar
+Install-ChocolateyPinnedTaskBarItem "$($Boxstarter.programFilesx86)\Google\Chrome\Application\chrome.exe"
 #pin powershell to taskbar
+Install-ChocolateyPinnedTaskBarItem "$env:windir\system32\WindowsPowerShell\v1.0\powershell.exe"
 #pin outlook 2010 to taskbar
 Install-ChocolateyPinnedTaskBarItem "${env:ProgramFiles(x86)}\Microsoft Office\Office14\OUTLOOK.EXE"
 #remove unwanted apps from win10
-#remote desktop manager to taskbar
+
+#pin remote desktop manager to taskbar
+Install-ChocolateyPinnedTaskBarItem "$($Boxstarter.programFilesx86)\Microsoft\Remote Desktop Connection Manager\RDCMan.exe"
 #windows feature Remote Server Administration
-#Remote Server Admin Tools for Windows 10 install
+Install-WindowsFeature -name "Remote Server Administration Tools"
